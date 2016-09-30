707771f8d3520d70a16237afa42a703f3efbce65
Rizwan1997 <rizwanparvez1@gmail.com>
2016-10-01 00:06:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,6 @@ var cnt = document.getElementById('count');
 var counter= 0;
 button.onclick=function(){
    counter = counter+1;
-   cnt.innerHTML= res.send(counter.toString());
+   cnt.innerHTML= counter.toString();
 };
     
