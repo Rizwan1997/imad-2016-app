0153adb17ea43f3f3471bebf148bdd029c2e43f8
Rizwan1997 <rizwanparvez1@gmail.com>
2016-10-01 00:42:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,6 +15,6 @@ button.onclick=function(){
        }
    };
    request.open('GET','http://rizwan1997.imad.hasura-app.io/counter',true);
-   request.send(null);
+   request.send(counter);
 };
     
