5a2adf987fb42ba77235203077cbcc15a9098f9a
Rizwan1997 <rizwanparvez1@gmail.com>
2016-09-30 22:14:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,13 @@
 console.log('Loaded!');
+
+var img=document.getElementbyId('madi');
+
+    var marginLeft=0;
+    function moveright(){
+        marginLeft=marginLeft+10;
+        img.style.marginLeft=marginLeft+'px';
+        
+    }
+img.onclick=function(){
+    var interval=setInterval(moveright,50);
+}
