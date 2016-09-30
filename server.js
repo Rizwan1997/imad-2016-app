bcb7d12bfb425b9a96b447c0b76305e3fa4ac0ef
Rizwan1997 <rizwanparvez1@gmail.com>
2016-09-30 21:47:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,55 +13,6 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-var articles = {
-    'article-one': {
-        title: 'Article one',
-        heading: 'Article one',
-        date: '26/09/2016',
-        content: `<!doctype html>
-                <html>
-                <head>
-                <title>ART ONE</title>
-                </head>
-                <link href="/ui/style.css" rel="stylesheet" />
-                <body>
-                    <a href="/">HOME</a>
-                     <h2>26/09/2016</h2>
-                <div class/"center">
-                    <h2>Article Two</h2>
-                </div> <hr/>
-                <p> This is the content of my first article on page <b>/article-one</b>. Continue reading Continue reading Continue reading Continue reading </p> 
-                <img src="/ui/madi.png" class="img-small"/>
-                </body>
-            </html> `
-     } };
-     
-    
-
-function createtemplate(data){
-    var title = data.title;
-    var heading = data.heading;
-    var date = data.date;
-    var htmltemplate = `
-    <!doctype html>
-                <html>
-                <head>
-                <title>$(title)</title>
-                </head>
-                <link href="/ui/style.css" rel="stylesheet" />
-                <body>
-                    <a href="/">HOME</a>
-                   <h2>$(date)</h2> 
-                <div class="center">
-                    <h2>$(heading)</h2>
-                </div> <hr/>
-                <p> This is the content of my first article on page <b>/article-one</b>. Continue reading Continue reading Continue reading Continue reading </p> 
-                <img src="/ui/madi.png" class="img-small"/>
-                </body>
-            </html> `
-            return htmltemplate;
-    }
-
 app.get('/:articleName', function(res,req){
     var ArticleName = req.params.articleName;
     res.send(createtemplate(articles[ArticleName]));
