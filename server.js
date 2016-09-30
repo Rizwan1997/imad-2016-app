43939e5dcec87c96d00893d9189ebe2952a1aa0b
Rizwan1997 <rizwanparvez1@gmail.com>
2016-09-30 23:31:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,6 +23,10 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+app.get('/ind2', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index2.html'));
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
