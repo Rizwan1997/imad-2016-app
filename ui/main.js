6456fd52a21bf140c2c32494d96cc337d0317423
Rizwan1997 <rizwanparvez1@gmail.com>
2016-11-01 23:15:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,19 @@
 var button = document.getElementById("counter");
-var counter;
+
 button.onclick = function(){
-    counter = counter + 1;
-    var span = document.getElementById("count");
-    span.innerHTML = counter.toString();
-}
+    
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readystate === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById("count");
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+   
+    request.open('GET', 'http://rizwan1997.imad.hasura-app.io/counter', true);
+    request.send(null);
+};
 
