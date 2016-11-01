9595198c01492421a548af6fc3ba66242a312206
Rizwan1997 <rizwanparvez1@gmail.com>
2016-11-01 23:20:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-var button = document.getElementById("counter");
+var button = document.getElementById('counter');
 
 button.onclick = function(){
     
@@ -7,7 +7,7 @@ button.onclick = function(){
         if(request.readystate === XMLHttpRequest.DONE){
             if(request.status === 200){
                 var counter = request.responseText;
-                var span = document.getElementById("count");
+                var span = document.getElementById('count');
                 span.innerHTML = counter.toString();
             }
         }
