1b50eb5ceff1f1769512e874ad881141e5a84acb
Rizwan1997 <rizwanparvez1@gmail.com>
2016-11-02 15:52:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,22 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/style1.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style1.css'));
+});
+
+app.get('/ui/style2.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style2.css'));
+});
+
+app.get('/ui/style3.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style3.css'));
+});
+
+app.get('/ui/style4.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style4.css'));
+});
+
 var counter=0;
 app.get('/counter', function(req,res){
     counter=counter+1;
@@ -23,10 +39,21 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ind2', function (req, res) {
-  res.sendFile(path.join(__dirname, 'index2.html'));
+app.get('/profile', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'profile.html'));
 });
 
+app.get('/contact', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'contact.html'));
+});
+
+app.get('/Blog_comment', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'Blog_comment'));
+});
+
+app.get('/blog_posts', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'blog_posts'));
+});
 var names= [];
 app.get('/submit-name', function (req, res) {
   var name= req.query.name;
@@ -38,6 +65,29 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/ui/me.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+app.get('/ui/me1.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+app.get('/ui/main.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+app.get('/ui/profile.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+app.get('/ui/star.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+app.get('/ui/image.jpeg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
